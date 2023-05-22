2e2097b1612c8f59c87d78b848042226f7d16b4a
Isabella-SH <sorianoisabella28@gmail.com>
2023-05-21 22:29:15 -0500

    feat: created Skill entity. Relationship between Skill and Criterion

--- src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
@@ -1,7 +1,6 @@
 package com.acme.learning.platform.learning.domain.model;
 
 import com.acme.learning.platform.shared.domain.model.AuditModel;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,19 +10,19 @@ import lombok.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
+
+
 @With
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity //le decimos a JPA "este entity esta vinculado a una creacion de tabla"
+@Entity
 
 //crea una tabla de esta entidad, siempre incluir el id dentro
 //pero ello lo señalaremos mas abajo
-@Table(name="criteria")//fuerzo el "name", para respetar las convenciones, el nombre de las tablas son en plural
+@Table(name="skills")//fuerzo el "name", para respetar las convenciones, el nombre de las tablas son en plural
 //sin embargo, nombre de atributos es en singular
 
-public class Criterion extends AuditModel {
+public class Skill extends AuditModel {
 
     //la tarea de generar el id recae en estas definiciones
     //señalamos que el atributo "id", sera lo de la tabla
@@ -32,20 +31,42 @@ public class Criterion extends AuditModel {
     private Long id;
 
     //el atributo "name" no ha de ser nulo ni vacio
+    //de tamaño max 60
     @NotNull
     @NotBlank
+    @Size(max=60)
     private String name;
 
     //Relationships
 
-    //"ManyToOne": entre criterion y skill establecemos una relacion de uno a muchos
+    //"OneToMany": entre skill y criterion establecemos una relacion de uno a muchos
+    //"cascade": la relacion se aplica en cascada. Por ejemplo, si elimino un skill, elimino todos los criterios asociados a el
     //"fetch": tiene que ver la con la recuperacion
     //"fetch = FetchType.EAGER": cuando recupero un skill, la coleccion criteria quiero que se vaya a poblar con_todo lo que tengan asociado ese skill, para que siempre que la consulte este con valores
-    //Cuando yo recupero un objeto de la clase Criterion automaticamente recupera a que skill pertenece
-    //"optional=false": no debe guardar un criterio sin skill
-    @ManyToOne(fetch = FetchType.EAGER,optional = false)
-    @JoinColumn(name="skill.id",nullable=false)
-    @JsonIgnore //por si se crea una coleccion json de Criterion, entonces esta bno debe aparecer, solo deberia aparecer "criteria"
-    private Skill skill;
-
+    //"mappedBy = "skill": Criterion debera tener una propiedad llamada skill, para poder navegar
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "skill")
+    private Set<Criterion> criteria=new HashSet<>();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
