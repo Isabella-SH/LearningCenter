ec5d33ce2eb9135de2a1a1565bb04d8834fd3a28
Isabella-SH <sorianoisabella28@gmail.com>
2023-05-21 22:03:19 -0500

    feat: created Criterion entity and edit Student entity

--- src/main/java/com/acme/learning/platform/learning/domain/model/Criterion.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/Criterion.java
@@ -12,16 +12,14 @@ import lombok.*;
 @With
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity //le decimos a JPA "este entity esta vinculado a una creacion de tabla"
 
 //crea una tabla de esta entidad, siempre incluir el id dentro
 //pero ello lo señalaremos mas abajo
-@Table(name="students")//fuerzo el "name", para respetar las convenciones, el nombre de las tablas son en plural
+@Table(name="criteria")//fuerzo el "name", para respetar las convenciones, el nombre de las tablas son en plural
 //sin embargo, nombre de atributos es en singular
 
-
-//extiende de la clase abstracta
-public class Student extends AuditModel {
+public class Criterion extends AuditModel {
 
     //la tarea de generar el id recae en estas definiciones
     //señalamos que el atributo "id", sera lo de la tabla
@@ -30,15 +28,8 @@ public class Student extends AuditModel {
     private Long id;
 
     //el atributo "name" no ha de ser nulo ni vacio
-    //de tamaño max 60 y debe ser unico en relacion con los ya existentes
     @NotNull
     @NotBlank
-    @Size(max=60)
-    @Column(unique=true)
     private String name;
 
-    private int age;
-
-    @Size(max=240)
-    private String address;
 }
