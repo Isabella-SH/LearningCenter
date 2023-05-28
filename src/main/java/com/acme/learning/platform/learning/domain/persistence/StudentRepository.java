27ed08d0cd3a3151fa18f35ca4867c54273ad267
Isabella-SH <sorianoisabella28@gmail.com>
2023-05-28 02:53:15 -0500

    feat: added student repository

---
+++ src/main/java/com/acme/learning/platform/learning/domain/persistence/StudentRepository.java
@@ -0,0 +1,20 @@
+//esta interfaz no es necesaria implementarla,
+//solo esta aqui para respetar el patron de responsabilidad unica
+
+package com.acme.learning.platform.learning.domain.persistence;
+
+import com.acme.learning.platform.learning.domain.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,long> {
+
+    //realizo las consultas con los atributos que quiero
+
+    List <Student> findAllBy(int age);
+
+    Student findByName(String name);
+}
