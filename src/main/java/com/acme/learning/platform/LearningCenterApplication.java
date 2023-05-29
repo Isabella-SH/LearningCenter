7159de87eddc532f0324c2fc4b65478a9ff6f3f6
Isabella-SH <sorianoisabella28@gmail.com>
2023-05-28 22:20:58 -0500

    chore: Enabled JPA Auditing

--- src/main/java/com/acme/learning/platform/LearningCenterApplication.java
+++ src/main/java/com/acme/learning/platform/LearningCenterApplication.java
@@ -2,7 +2,9 @@ package com.acme.learning.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //activa la superclass declarada en shared->domain->model->auditModel
 @SpringBootApplication
 public class LearningCenterApplication {
 
