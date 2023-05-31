53de8022ccafb4a14acf34f4175fbe1e02f07ef5
Isabella-SH <sorianoisabella28@gmail.com>
2023-05-31 18:45:52 -0500

    feat: added behavior to skill

--- src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
@@ -1,6 +1,7 @@
 package com.acme.learning.platform.learning.domain.model;
 
 import com.acme.learning.platform.shared.domain.model.AuditModel;
+import com.acme.learning.platform.shared.exception.ResourceValidationException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -45,7 +46,52 @@ public class Skill extends AuditModel {
     //"fetch = FetchType.EAGER": cuando recupero un skill, la coleccion criteria quiero que se vaya a poblar con_todo lo que tengan asociado ese skill, para que siempre que la consulte este con valores
     //"mappedBy = "skill": Criterion debera tener una propiedad llamada skill, para poder navegar
     @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "skill")
-    private Set<Criterion> criteria=new HashSet<>();
+    private Set<Criterion> criteria;
+
+    //la coleccion de criterion es una propiedad de esta clase
+
+    //considerar que esto es un agregate: un skill tiene muchos criterion
+    //los agregate deben tener logica de negocio incorporada
+
+    //realiza este metodo aplicando reglas de negocio
+    public Skill addCriterion(String criterionName){
+
+        if(criteria==null){
+            criteria=new HashSet<>();
+        }
+
+        //validate Criterion Name Uniqueness for Skill
+        //valido el nombre del criterion, si es elmismo, entonces es un error
+        //solo puede existir el mismo nombre de criterion si esta en diferentes skills
+        if(!criteria.isEmpty()){
+            if (criteria.stream().anyMatch(criterion -> criterion.getName().equals(criterionName)))
+                throw new ResourceValidationException("Criterion", "A criterion with the same name alredy exists.");
+        }
+
+        //add Criterion to Skill
+        criteria.add(new Criterion()
+                .withName(criterionName)
+                .withSkill(this));
+
+        return this;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
