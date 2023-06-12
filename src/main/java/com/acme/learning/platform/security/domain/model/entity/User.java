fc750a2c90a067622ba9102610d509f96d938390
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-11 19:49:25 -0500

    fix: removed extension from AuditModel

--- src/main/java/com/acme/learning/platform/security/domain/model/entity/User.java
+++ src/main/java/com/acme/learning/platform/security/domain/model/entity/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @Entity
 @Table(name="users")
 
-public class User extends AuditModel {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
